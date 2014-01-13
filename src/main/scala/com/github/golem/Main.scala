c8de765a231af299a33c8987ca3e6c62de7a6313
Piotr Jarosik <jarosik.piotr@gmail.com>
2014-01-13 20:38:48 +0100

    Minor fix.

--- src/main/scala/com/github/golem/Main.scala
+++ src/main/scala/com/github/golem/Main.scala
@@ -8,7 +8,7 @@ object Main {
 
   def main(args: Array[String]) {
     if(args.length < 2) {
-      System.err.println("You should provide exactly two parameters:" +
+      throw new IllegalArgumentException("You should provide exactly two parameters:" +
         " path to kgs client properties file, path to log file")
     }
     val system = ActorSystem()
